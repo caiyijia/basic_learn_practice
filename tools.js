f36f34670d2cbab009c0d1b89ec83105610e4949
Adam Cai <caiyijias@outlook.com>
2019-05-31 19:03:23 +0800

    事事件1

---
+++ tools.js
@@ -0,0 +1,22 @@
+// 兼容的事件绑定
+function addEvent(elem, type, handle) {
+    if (elem.addEventListener) {
+        elem.addEventListener(type, handle, false);
+    } else if (elem.attachEvent) {
+        elem.attachEvent('on' + type, function () {
+            // 此方法下this指向window
+            handle.call(elem);
+        })
+    } else {
+        elem['on' + type] = handle;
+    }
+}
+
+ // 阻止默认事件的函数
+ function cancelHandler(event) {
+    if(event.preventDefault) {
+        event.preventDefault();
+    }else {
+        event.returnValue = false;
+    }
+}
